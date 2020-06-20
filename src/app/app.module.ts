9f4ea38518c9a9c4529a04f5ddd63bdd55a7d1bf
Ahmad Mujtaba <888134@mohre.gov.ae>
2020-06-20 18:08:21 +0400

    weather service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { AddCardComponent } from "./add-card/add-card.component";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { AddCardComponent } from "./add-card/add-card.component";
     AddCardComponent,
     WeatherCardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
